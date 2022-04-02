8263b44cfcd28828835c05593848370a48379551
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-04-01 21:00:13 -0300

    segundo commit 3f

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 se debe hacer?¿Cómo configuraría el archivo .gitignore?
 
 indica cuales archivos no se van a subir al repositorio, por ejemplo los .exe de los codigos
+
+//3.c) Revise los archivos subidos en su repositorio de github. ¿Que archivos
+ve?¿Cuál cree que no hace falta que esté?
+
+.exe esta de mas
+
+
--- tp1_1.c
+++ tp1_1.c
@@ -5,6 +5,18 @@ int main (){
 
     printf("Hola Mundo");
 
+    int var = 3;
+    int *p_var;
+
+    p_var = &var;
+
+    printf("el contenido del puntero es: %d \n", p_var);
+    printf("la direccion de memoria almacenada en el puntero es: %d \n", p_var);
+    printf("la direccion de memoria almacenada de la variable es: %d \n", &var);
+    printf("la direccion de memoria almacenada del puntero es: %d \n", &p_var);
+    printf("el tamano de memoria utilizado por esa variable es: %d \n", sizeof(var));
+
+
 
 
     return (0);
